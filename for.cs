22512d9b36397b9fc1e6834af87c9069c66ce901
Prtk98 <116975693+Prtk98@users.noreply.github.com>
2022-10-30 00:57:12 +0530

    Create for.cs

---
+++ for.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace prtk
+{
+  class csharplearn
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 7; i++) 
+      {
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
